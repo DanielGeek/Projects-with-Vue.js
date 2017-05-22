e1912161f5ae5f560c02f0c235f460040e970388
daniel angel <daniel30081990@gmail.com>
2017-05-22 18:17:26 -0400

    Dynamic Styling with CSS Classes - Using Objects

--- introduction/js/css_class.js
+++ introduction/js/css_class.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#app',
     data: {
         attachAqua: false
+    },
+    computed: {
+        divClasses: function () {
+            return {
+                aqua: this.attachAqua,
+                blue: !this.attachAqua
+            }
+        }
     }
 });
