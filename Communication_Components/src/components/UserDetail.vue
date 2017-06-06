90b2424a84f30a043a99dbe7adf0bf8300da40ac
daniel angel <daniel30081990@gmail.com>
2017-06-06 00:11:33 -0400

    Using Custom Events for Child => Parent Communic

--- Communication_Components/src/components/UserDetail.vue
+++ Communication_Components/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
   <h3>You may view the User Details here</h3>
   <p>Many Details</p>
   <p>User Name: {{ switchName() }}</p>
+  <button type="button" name="button" @click="resetName">Reset Name</button>
 </div>
 </template>
 
@@ -16,6 +17,10 @@ export default {
   methods: {
     switchName() {
       return this.myName.split("").reverse().join("");
+    },
+    resetName() {
+      this.myName = 'Daniel';
+      this.$emit('nameWasReset', this.myName);
     }
   }
 }
