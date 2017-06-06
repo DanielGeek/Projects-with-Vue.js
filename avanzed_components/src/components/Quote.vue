62e48729ec9d3aa64c8462efabac6e50a23dfff2
daniel angel <daniel30081990@gmail.com>
2017-06-06 17:47:55 -0400

    Create Quote in proyecto

---
+++ avanzed_components/src/components/Quote.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <p>A wonderful Quote!</p>
+</div>
+</template>
+
+<scrip>
+
+</scrip>
+
+<style scoped>
+div {
+  boder: 1px solid #ccc;
+  box-shadow: 1px 1px 2px black;
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+}
+</style>
