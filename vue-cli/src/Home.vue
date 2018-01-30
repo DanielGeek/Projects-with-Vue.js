9a13e5705bf37d35aa531b1925755c851b0faae4
DanielGeek <daniel30081990@gmail.com>
2018-01-30 00:33:58 -0400

    Simple componente

--- vue-cli/src/Home.vue
+++ vue-cli/src/Home.vue
@@ -1,14 +1,31 @@
 <template lang="html">
-  <div>
-    <app-server-status v-for="server in 5"></app-server-status>
+  <div id="app">
+    <my-cmp></my-cmp>
+    <hr>
+    <my-cmp></my-cmp>
+    <!-- <app-server-status v-for="server in 5"></app-server-status> -->
   </div>
 </template>
 
 <script>
-import ServerStatus from './ServerStatus.vue';
-export default {
-  components: {
-    'app-server-status': ServerStatus
+import Vue from 'vue'
+import App from './Home.vue'
+
+// var data = { status: 'Critical' };
+
+ Vue.component('my-cmp', {
+  data: function() {
+    return {
+      status: 'Critical'
+    };
+  },
+  template: '<p>Server Status: {{ status }} (<button @click="changeStatus">Change</button></p>',
+  methods: {
+    changeStatus: function() {
+      this.status = 'Normal';
+    }
   }
-}
+});
+
+
 </script>
